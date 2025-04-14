70ecdc285d37cc3fb657e08582c984b79d8a21a9
womendoushihaoyin <xyliu_leo@163.com>
2025-04-14 14:51:07 +0800

    fix linux and mac dep compilation

--- deps/MPFR/MPFR.cmake
+++ deps/MPFR/MPFR.cmake
@@ -26,7 +26,7 @@ else ()
 
     ExternalProject_Add(dep_MPFR
         URL https://www.mpfr.org/mpfr-current/mpfr-4.2.2.tar.bz2
-        URL_HASH 9ad62c7dc910303cd384ff8f1f4767a655124980bb6d8650fe62c815a231bb7b
+        URL_HASH SHA256=9ad62c7dc910303cd384ff8f1f4767a655124980bb6d8650fe62c815a231bb7b
         DOWNLOAD_DIR ${DEP_DOWNLOAD_DIR}/MPFR
         BUILD_IN_SOURCE ON
         CONFIGURE_COMMAND autoreconf -f -i && 

3e0c9bb0f2d071d829bc61b2ded20403fb959fa2
xiaoyeliu <xyliu_leo@163.com>
2025-08-15 11:12:24 +0800

    bump: bump flutter version to 1.0.7

--- resources/web/flutter_web/flutter_bootstrap.js
+++ resources/web/flutter_web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2267248759"
+    serviceWorkerVersion: "3214748554"
   }
 });

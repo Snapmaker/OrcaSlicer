2572023181f0ef732df444ee79ac731a949fbbc3
goofoo <goofoo2@gmail.com>
2026-03-10 09:14:13 -0400

    Fix issue #161: Implement spiral lift bed boundary check in GCodeWriter
    Add BoundingBoxf m_bed_bbox member to track the print bed extents for
    use in spiral lift boundary checking.

--- src/libslic3r/GCodeWriter.hpp
+++ src/libslic3r/GCodeWriter.hpp
@@ -6,6 +6,7 @@
 #include <charconv>
 #include "Extruder.hpp"
 #include "Point.hpp"
+#include "BoundingBox.hpp"
 #include "PrintConfig.hpp"
 #include "GCode/CoolingBuffer.hpp"
 
@@ -158,6 +159,8 @@ public:
     //BBS: x, y offset for gcode generated
     double          m_x_offset{ 0 };
     double          m_y_offset{ 0 };
+    //BBS: bed bounding box for spiral lift boundary check
+    BoundingBoxf    m_bed_bbox;
     
     std::string m_gcode_label_objects_start;
     std::string m_gcode_label_objects_end;

ae010717a28f48b18b70ec4f5569b8226187584e
alves <liulikeqian@gmail.com>
2026-04-24 12:17:04 +0800

    feature add file exist check for font file.

--- src/slic3r/GUI/Widgets/Label.cpp
+++ src/slic3r/GUI/Widgets/Label.cpp
@@ -62,23 +62,31 @@ void Label::initSysFont()
 {
 #if defined(__linux__) || defined(_WIN32)
     const std::string &resource_path = Slic3r::resources_dir();
+    bool result = false;
+
     wxString font_path = wxString::FromUTF8(resource_path + "/fonts/HarmonyOS_Sans_SC_Bold.ttf");
-    bool result = wxFont::AddPrivateFont(font_path);
-    // BOOST_LOG_TRIVIAL(info) << boost::format("add font of HarmonyOS_Sans_SC_Bold returns %1%")%result;
-    printf("add font of HarmonyOS_Sans_SC_Bold returns %d\n", result);
+    if(boost::filesystem::exists(font_path.ToStdString())) {
+        result = wxFont::AddPrivateFont(font_path);
+        printf("add font of HarmonyOS_Sans_SC_Bold returns %d\n", result);
+    }
+
     font_path = wxString::FromUTF8(resource_path + "/fonts/HarmonyOS_Sans_SC_Regular.ttf");
-    result = wxFont::AddPrivateFont(font_path);
-    // BOOST_LOG_TRIVIAL(info) << boost::format("add font of HarmonyOS_Sans_SC_Regular returns %1%")%result;
-    printf("add font of HarmonyOS_Sans_SC_Regular returns %d\n", result);
-    // Adding NanumGothic Regular and Bold
+    if (boost::filesystem::exists(font_path.ToStdString())) {
+        result = wxFont::AddPrivateFont(font_path);    
+        printf("add font of HarmonyOS_Sans_SC_Regular returns %d\n", result);
+    }
+
     font_path = wxString::FromUTF8(resource_path + "/fonts/NanumGothic-Regular.ttf");
-    result = wxFont::AddPrivateFont(font_path);
-    // BOOST_LOG_TRIVIAL(info) << boost::format("add font of NanumGothic-Regular returns %1%")%result;
-    printf("add font of NanumGothic-Regular returns %d\n", result);
+    if (boost::filesystem::exists(font_path.ToStdString())) {
+        result = wxFont::AddPrivateFont(font_path);    
+        printf("add font of NanumGothic-Regular returns %d\n", result);
+    }
+
     font_path = wxString::FromUTF8(resource_path + "/fonts/NanumGothic-Bold.ttf");
-    result = wxFont::AddPrivateFont(font_path);
-    // BOOST_LOG_TRIVIAL(info) << boost::format("add font of NanumGothic-Bold returns %1%")%result;
-    printf("add font of NanumGothic-Bold returns %d\n", result);
+    if (boost::filesystem::exists(font_path.ToStdString())) {
+        result = wxFont::AddPrivateFont(font_path);
+        printf("add font of NanumGothic-Bold returns %d\n", result);
+    }
 #endif
     Head_48 = Label::sysFont(48, true);
     Head_32 = Label::sysFont(32, true);

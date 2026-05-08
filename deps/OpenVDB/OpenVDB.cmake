a60336c2730bd7a784ffa0409508809c0cc6c25f
alves <liulikeqian@gmail.com>
2026-05-08 18:55:21 +0800

    feature fix build question on local .

--- deps/OpenVDB/OpenVDB.cmake
+++ deps/OpenVDB/OpenVDB.cmake
@@ -6,6 +6,9 @@ else()
     set(_build_static ON)
 endif()
 
+# get relative path of CMAKE_BINARY_DIR against root source directory
+file(RELATIVE_PATH BINARY_DIR_REL  ${CMAKE_SOURCE_DIR}/.. ${CMAKE_BINARY_DIR})
+
 if (IN_GIT_REPO)
     set(OPENVDB_DIRECTORY_FLAG --directory ${BINARY_DIR_REL}/dep_OpenVDB-prefix/src/dep_OpenVDB)
 endif ()
@@ -14,6 +17,7 @@ Snapmaker_Orca_add_cmake_project(OpenVDB
     #  support vs2022, update to 8.2
     URL https://github.com/tamasmeszaros/openvdb/archive/a68fd58d0e2b85f01adeb8b13d7555183ab10aa5.zip 
     URL_HASH SHA256=f353e7b99bd0cbfc27ac9082de51acf32a8bc0b3e21ff9661ecca6f205ec1d81
+    PATCH_COMMAND ${CMAKE_COMMAND} -E echo "Patching OpenVDB..." || true
     DEPENDS dep_TBB dep_Blosc dep_OpenEXR dep_Boost
     CMAKE_ARGS
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON 
@@ -26,6 +30,25 @@ Snapmaker_Orca_add_cmake_project(OpenVDB
         -DOPENVDB_BUILD_VDB_PRINT=ON
         -DDISABLE_DEPENDENCY_VERSION_CHECKS=ON # Centos6 has old zlib
 )
+
+ExternalProject_Get_Property(dep_OpenVDB SOURCE_DIR)
+if (APPLE AND CMAKE_SYSTEM_PROCESSOR MATCHES "^(arm64|aarch64)")
+    # New portable logic only for macOS arm64.
+    ExternalProject_Add_Step(dep_OpenVDB fix_template_syntax
+        DEPENDEES configure
+        DEPENDERS build
+        COMMAND ${CMAKE_COMMAND}
+                -DOPENVDB_NODE_MANAGER_H=${SOURCE_DIR}/openvdb/openvdb/tree/NodeManager.h
+                -P ${CMAKE_CURRENT_LIST_DIR}/OpenVDB_fix_template_syntax.cmake
+    )
+else ()
+    # Keep original behavior on other platforms.
+    ExternalProject_Add_Step(dep_OpenVDB fix_template_syntax
+        DEPENDEES configure
+        DEPENDERS build
+        COMMAND bash -c "cd '${SOURCE_DIR}/openvdb/openvdb/tree' && sed -i '' 's|OpT::template eval|OpT::eval|g' NodeManager.h"
+    )
+endif ()
 
 if (MSVC)
     if (${DEP_DEBUG})

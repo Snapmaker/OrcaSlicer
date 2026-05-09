2b72199b538acd1c60b34f09564fb4cf4347f386
alves <liulikeqian@gmail.com>
2026-05-09 15:14:22 +0800

    feature changed the about me for flags the function .

--- src/slic3r/GUI/AboutDialog.cpp
+++ src/slic3r/GUI/AboutDialog.cpp
@@ -285,13 +285,8 @@ AboutDialog::AboutDialog()
     
 
     // SM Beta
-    text_list.push_back(_L("Note: The firmware version of SnapmakerU1 should be no less than") + " " + std::string(MIN_FIRM_VER));
-    text_list.push_back(_L("Snapmaker Orca is forked from Orca Slicer by SoftFever."));
-    text_list.push_back(_L("OrcaSlicer is forked from Bambu Studio by Bambu Lab."));
-    text_list.push_back(_L("Bambu Studio is forked from PrusaSlicer by Prusa Research, which is from Slic3r by Alessandro Ranellucci and the RepRap community. Orca Slicer incorporates a lot of features from SuperSlicer by @supermerill."));
-    text_list.push_back(_L("Snapmaker Orca integrates some features from the 3D printing open-source community, with specific attributions provided in the code comments."));
-    text_list.push_back(_L(""));
-    text_list.push_back(_L("Snapmaker Orca is licensed under the GNU Affero Generall Public License, version 3."));
+    text_list.push_back(_L("2026-5-9 基于外发版本开放gcode.3mf的导出功能包"));
+    
     
     text_sizer->Add( 0, 0, 0, wxTOP, FromDIP(33));
     bool is_zh = wxGetApp().app_config->get("language") == "zh_CN";

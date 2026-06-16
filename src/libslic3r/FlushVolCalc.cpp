54ffedb93f2f16e17f4d842ba93656064e3e676b
lujiaxin <1091917314qq.com>
2026-06-16 12:29:55 +0800

    V3 save gaurd opt

--- src/libslic3r/FlushVolCalc.cpp
+++ src/libslic3r/FlushVolCalc.cpp
@@ -36,7 +36,7 @@ static float DeltaHS_BBS(float h1, float s1, float v1, float h2, float s2, float
     float dx = std::cos(h1_rad) * s1 * v1 - cos(h2_rad) * s2 * v2;
     float dy = std::sin(h1_rad) * s1 * v1 - sin(h2_rad) * s2 * v2;
     float dxy = std::sqrt(dx * dx + dy * dy);
-    return std::min(0.8f, dxy);
+    return std::min(0.81f, dxy);
 }
 
 FlushVolCalculator::FlushVolCalculator(int min, int max, float multiplier)
@@ -77,18 +77,18 @@ int FlushVolCalculator::calc_flush_vol(unsigned char src_a, unsigned char src_r,
     float to_lumi = get_luminance(dst_r_f, dst_g_f, dst_b_f);
     float lumi_flush = 0.f;
     if (to_lumi >= from_lumi) {
-        lumi_flush = std::pow(to_lumi - from_lumi, 1.12f) * 150.f;
+        lumi_flush = std::pow(to_lumi - from_lumi, 0.97f) * 265.f;
     }
     else {
-        lumi_flush = (from_lumi - to_lumi) * 10.f;
+        lumi_flush = (from_lumi - to_lumi) * 8.f;
 
-        float inter_hsv_v = 0.92f * to_hsv_v + 0.08f * from_hsv_v;
+        float inter_hsv_v = 0.93f * to_hsv_v + 0.07f * from_hsv_v;
         hs_dist = std::min(inter_hsv_v, hs_dist);
     }
-    float hs_flush = 55.f * hs_dist;
+    float hs_flush = 95.f * hs_dist;
 
-    float flush_volume = calc_triangle_3rd_edge(hs_flush, lumi_flush, 115.f);
-    flush_volume = std::max(flush_volume, 32.f);
+    float flush_volume = calc_triangle_3rd_edge(hs_flush, lumi_flush, 118.f);
+    flush_volume = std::max(flush_volume, 60.f);
 
     //float flush_multiplier = std::atof(m_flush_multiplier_ebox->GetValue().c_str());
     // flush_volume += m_min_flush_vol;

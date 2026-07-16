cfea00521f611f5136c758443a0a71000bdef8ae
kenshin627 <87zhaoxiaobo@163.com>
2026-07-16 16:34:06 +0800

    [Fix]: macOS build error in CpuMemory - use Mach API instead of Linux sysinfo
    sys/sysinfo.h is Linux-only and sysconf(_SC_AVPHYS_PAGES) is unsupported on
    macOS. Split the __APPLE__ branch out from __linux__ and query free memory
    via host_statistics64 (vm_statistics64) instead.

--- src/slic3r/Utils/CpuMemory.cpp
+++ src/slic3r/Utils/CpuMemory.cpp
@@ -1,36 +1,78 @@
 #include "CpuMemory.hpp"
 
+namespace Slic3r {
 #ifdef _WIN32
 #include <windows.h>
-#elif defined(__APPLE__) || defined(__linux__)
+unsigned long long GetFreeMemoryWin()
+{
+    MEMORYSTATUSEX status;
+    status.dwLength = sizeof(status);
+    GlobalMemoryStatusEx(&status);
+    return status.ullAvailPhys;
+}
+#endif
+
+#ifdef __linux__
 #include <unistd.h>
 #include <sys/sysinfo.h>
+#elif __APPLE__
+#include <mach/mach_host.h>
+#include <sys/sysctl.h>
 #endif
+#if defined(__linux__) || defined(__APPLE__)
+unsigned long long GetFreMemoryUnix()
+{
+#ifdef __linux__
+    struct sysinfo info;
+    if (sysinfo(&info) == 0) {
+        return info.freeram * info.mem_unit;
+    }
+#elif __APPLE__
+    int      mib[2] = {CTL_HW, HW_MEMSIZE};
+    uint64_t memsize;
+    size_t   len = sizeof(memsize);
+    if (sysctl(mib, 2, &memsize, &len, NULL, 0) == 0) {
+        vm_size_t              page_size;
+        mach_port_t            mach_port;
+        mach_msg_type_number_t count;
+        vm_statistics64_data_t vm_stats;
 
-namespace Slic3r {
-
-bool CpuMemory::CurFreeMemoryLessThanSpecifySizeGb(int sizeGb)
+        mach_port = mach_host_self();
+        count     = sizeof(vm_stats) / sizeof(natural_t);
+        if (host_page_size(mach_port, &page_size) == KERN_SUCCESS && host_statistics64(mach_port, HOST_VM_INFO, (host_info64_t) &vm_stats, &count) == KERN_SUCCESS) {
+            return (vm_stats.free_count + vm_stats.inactive_count) * page_size;
+        }
+    }
+#endif
+    return 0;
+}
+#endif
+unsigned long long get_free_memory()
 {
 #ifdef _WIN32
-    MEMORYSTATUSEX memStatus;
-    memStatus.dwLength = sizeof(MEMORYSTATUSEX);
-    if (GlobalMemoryStatusEx(&memStatus)) {
-        unsigned long long freeMemGb = memStatus.ullAvailPhys / (1024ULL * 1024ULL * 1024ULL);
-        return freeMemGb < static_cast<unsigned long long>(sizeGb);
-    }
-    return false; // Can't determine, default to not blocking
-#elif defined(__APPLE__) || defined(__linux__)
-    // Linux and macOS
-    long pages = sysconf(_SC_AVPHYS_PAGES);
-    long pageSize = sysconf(_SC_PAGESIZE);
-    if (pages > 0 && pageSize > 0) {
-        unsigned long long freeMemGb = (unsigned long long)pages * (unsigned long long)pageSize / (1024ULL * 1024ULL * 1024ULL);
-        return freeMemGb < static_cast<unsigned long long>(sizeGb);
-    }
-    return false; // Can't determine, default to not blocking
+    return GetFreeMemoryWin();
+#elif defined(__linux__) || defined(__APPLE__)
+    return GetFreMemoryUnix();
 #else
-    return false;
+    return 0;
 #endif
 }
+bool CpuMemory::CurFreeMemoryLessThanSpecifySizeGb(int size)
+{
+    unsigned long long free_mem = get_free_memory();
+    auto cur_size = free_mem / (1024.0 * 1024.0 * 1024.0);
+    static bool first_debug_free_memory = true;
+    static bool first_meet_size_gb      = true;
+    if (first_debug_free_memory) {
+        first_debug_free_memory = false;
+    }
+    if (cur_size < size) {
+        if (first_meet_size_gb) {
+            first_meet_size_gb = false;
+        }
+        return true;
+    }
+    return false;
+}
 
-} // namespace Slic3r
+}

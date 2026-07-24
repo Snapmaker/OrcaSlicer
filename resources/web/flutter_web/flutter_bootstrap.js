3e0db85ffa3987c6913e8ffc3a50246342e94f86
SukiSunYuhang <sukiyuhang.sun@gmail.com>
2026-07-24 18:44:30 +0800

    update flutter_web to flutter_web_release_2.3.31+20260724183914

--- resources/web/flutter_web/flutter_bootstrap.js
+++ resources/web/flutter_web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"b8800d88be4866db1b15f8b954ab2573bba996
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "670774203"
+    serviceWorkerVersion: "98893131"
   }
 });

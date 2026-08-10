33640761e157ffafdcf8239f2e4f04c588d29f0a
SukiSunYuhang <sukiyuhang.sun@gmail.com>
2026-08-10 10:52:00 +0800

    @ (#684)
    feat: add sw_PrinterDefectDetection WCP pass-through interface
    
    Add sw_PrinterDefectDetection command in SSWCP_MachineOption_Instance
    that transparently forwards params to printer.defect_detection.config
    via async_defect_detaction_config.
    @

--- src/slic3r/GUI/SSWCP.cpp
+++ src/slic3r/GUI/SSWCP.cpp
@@ -2244,6 +2244,8 @@ void SSWCP_MachineOption_Instance::process()
         sw_ServerClientManagerSetUserinfo();
     } else if (m_cmd == "sw_DefectDetactionConfig"){
         sw_DefectDetactionConfig();
+    } else if (m_cmd == "sw_PrinterDefectDetection") {
+        sw_PrinterDefectDetection();
     }
     else if (m_cmd == GET_DEVICEDATA_STORAGESPACE) {
         sw_GetDeviceDataStorageSpace();
@@ -4062,6 +4064,26 @@ void SSWCP_MachineOption_Instance::sw_DefectDetactionConfig()
     } catch (std::exception& e) {
         handle_general_fail();
     }
+}
+
+void SSWCP_MachineOption_Instance::sw_PrinterDefectDetection()
+{
+    std::shared_ptr<PrintHost> host = nullptr;
+    wxGetApp().get_connect_host(host);
+
+    if (!host) {
+        BOOST_LOG_TRIVIAL(error) << "[WCP] sw_PrinterDefectDetection: no active machine connected";
+        handle_general_fail(-1, "Connection lost!");
+        return;
+    }
+
+    auto weak_self = std::weak_ptr<SSWCP_Instance>(shared_from_this());
+    host->async_defect_detaction_config(m_param_data, [weak_self](const json& response) {
+        auto self = weak_self.lock();
+        if (self) {
+            SSWCP_Instance::on_mqtt_msg_arrived(self, response);
+        }
+    });
 }
 
 void SSWCP_MachineOption_Instance::sw_GetFileListPage()
@@ -6332,6 +6354,7 @@ std::unordered_set<std::string> SSWCP::m_machine_option_cmd_list = {
     "sw_GetCameraTimelapseInstance",
     "sw_ServerClientManagerSetUserinfo",
     "sw_DefectDetactionConfig",
+    "sw_PrinterDefectDetection",
     GET_DEVICEDATA_STORAGESPACE
 };
 
--- src/slic3r/GUI/SSWCP.hpp
+++ src/slic3r/GUI/SSWCP.hpp
@@ -432,6 +432,7 @@ private:
     void sw_GetCameraTimelapseInstance();
 
     void sw_DefectDetactionConfig();    
+    void sw_PrinterDefectDetection();
 
     void sw_GetDeviceDataStorageSpace();
 
